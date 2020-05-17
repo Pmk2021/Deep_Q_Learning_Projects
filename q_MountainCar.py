f1959193fd822eab2b42b0974acb4a34415f2f06
Pmk2021 <pranavmk2003@gmail.com>
2020-05-17 10:36:27 -0400

    Update q_MountainCar.py

--- q_MountainCar.py
+++ q_MountainCar.py
@@ -8,19 +8,18 @@ import torch.nn.functional as F
 from random import randint
 import torch.optim as optim
 from copy import deepcopy
-import multiprocessing
-import trainprogram
+import multiprocessing as mp
 from sklearn.preprocessing import Normalizer
 
 loss = 0
 
-
+pool = mp.Pool(mp.cpu_count())
 
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.l1 = nn.Linear(2,59)
-        self.l3 = nn.Linear(59,3)
+        self.l1 = nn.Linear(4,50)
+        self.l3 = nn.Linear(50,2)
     def forward(self, x):
         x = F.relu(self.l1(x))
         x = self.l3(x)
@@ -39,14 +38,18 @@ class data():
         self.pastactions.append(action)
         self.reward.append(reward)
     def trainrandomdata(self):
-        i = randint(0,len(self.states)-2)
+        i = randint(0,len(self.states)-1)
+        if i == len(self.states)-1:
+            Q = Q_Net(torch.from_numpy(np.array(self.states[i])).float())
+            action = self.pastactions[i]
+            return (Q[action] - self.reward[i])**2
         Q = Q_Net(torch.from_numpy(np.array(self.states[i])).float())
-        Q_Target = Target_Net(torch.from_numpy(np.array(self.states[i])).float())
+        #Q_Target = Target_Net(torch.from_numpy(np.array(self.states[i])).float())
         action = self.pastactions[i]
         MaxQIndex = list(Q_Net(torch.from_numpy(np.array(self.states[i + 1])).float()).float().detach())
         MaxQIndex = MaxQIndex.index(max(MaxQIndex))
-        Q_Target[action] = 0.99 * Target_Net(torch.from_numpy(np.array(self.states[i+1])).float())[MaxQIndex] + self.reward[i + 1]
-        return ((Q-Q_Target)[action])**2
+        Q_Target = 0.85 * Target_Net(torch.from_numpy(np.array(self.states[i+1])).float())[MaxQIndex] + self.reward[i]
+        return ((Q[action]-Q_Target))**2
         #print(loss)
     def trainall(self):
         loss0 = 0
@@ -97,28 +100,27 @@ class data():
         return len(self.states)
     def trainlast(self):
         i = -2
-        Q = Q_Net(torch.from_numpy(np.array(self.states[i])).float())
-        Q_Target = Target_Net(torch.from_numpy(np.array(self.states[i])).float())
         action = self.pastactions[i]
+        Q = Q_Net(torch.from_numpy(np.array(self.states[i])).float())[action]
         MaxQIndex = list(Q_Net(torch.from_numpy(np.array(self.states[i + 1])).float()).float().detach())
         MaxQIndex = MaxQIndex.index(max(MaxQIndex))
-        Q_Target[action] = 0.99 * Target_Net(torch.from_numpy(np.array(self.states[i+1])).float())[MaxQIndex] + self.reward[i + 1]
-        return ((Q-Q_Target)[action])**2
+        Q_Target  = 0.85 * Target_Net(torch.from_numpy(np.array(self.states[i+1])).float())[MaxQIndex] + self.reward[i + 1]
+        return ((Q-Q_Target))**2
 
         
 Q_Net = Net()
 Target_Net = Net()
 
 #Q_Net.load_state_dict(torch.load(r'C:\Users\pmuralikrishnan\Desktop\Python\New folder\New folder\MountainCar_Q_Policy'))
-Q_Net.eval()
+#Q_Net.eval()
 
 #Target_Net.load_state_dict(torch.load(r'C:\Users\pmuralikrishnan\Desktop\Python\New folder\New folder\MountainCar_Q_Policy'))
-Target_Net.eval()
+#Target_Net.eval()
 
 Q_Data = data()
 
-optimizer = optim.Adam(Q_Net.parameters(), lr=.0)
-scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.9)
+optimizer = optim.Adam(Q_Net.parameters(), lr=.01)
+scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.99)
 
 c = 0.1
 
@@ -151,7 +153,7 @@ def maxlist(l1, l2):
 def getrandomloss(t):
     return datalist[randint(0,len(datalist)-1)].trainrandomdata()
 
-prob1 = 0
+prob1 = 30
 prob2 = 100
 epochs = 100
 ep_len = 3000
@@ -162,51 +164,54 @@ norm_list2 = list()
 max_point = -0.5
 
 a = 200
+av_len = 0
 for j in range(300000):
-    env = gym.make('MountainCar-v0')
-    env.reset()
-    s1 = [0,0]
+    env = gym.make('CartPole-v1')
+    s1 = env.reset()
     datalist.append(data())
     complete = False
+    far_dist = -100
+    fin = False
     for i in range(a):
-        env.render()
         q_vals = list(Q_Net(torch.from_numpy(np.array(s1)).float()))
-        if i%2 == 0:
-            if randint(0,100) > prob1:
-                action = q_vals.index(max(q_vals))
-            else:
-                action = randint(0,2)
+        if randint(0,100) > prob1:
+            action = q_vals.index(max(q_vals))
+        else:
+            action = randint(0,1)
         state, r, done, info = env.step(action)
+        if j%10000000 == 0 and 1 == 3:
+            env.render()
         s0 = s1
         s1 = state
-        reward =  1.7 + s1[0]
-        if s1[0] > 0.5:
-            reward = reward + 1
-        datalist[-1].newaction(action, reward, s1)
-        if i > 2 and i%10 == 0:
-            for k in range(32):
-                loss = loss + getrandomloss(1)
-            optimizer.zero_grad()
-            loss.backward(retain_graph=True)
-            optimizer.step()
+        datalist[-1].newaction(action, r, s0)
+        if done:
+            r = -5
+        if i > 2 and i%2 == 0:
             loss = 0
-        if s1[0] > 0.5:
-            print('Win')
-            print(str(j))
-            prob1 = prob1 * 0.95
-            scheduler.step()
-            complete = True
+            for k in range(1):
+                loss = loss + getrandomloss(1)
+                #loss = sum([pool.apply(getrandomloss, args=(row)) for row in range(16)])
+            optimizer.zero_grad()
+            loss.backward(retain_graph=False)
+            optimizer.step()
+        if done:
+            av_len += i
+            fin = True
             break
     env.close()
-    if not complete:
-        print('Lose')
-        print(str(j))
-    print(loss)
-    if j%10 == 0:
+    if not fin:
+        print('win')
+        scheduler.step()
+        prob1 = prob1 * 0.95
+        av_len += a
+    #print(loss)
+    if j%100 == 0:
         Target_Net = deepcopy(Q_Net)
     a = 200
-    print(loss)
-        
-
+    prob1 = prob1 * 0.999
+    if j%100 == 0:
+        print(av_len/100)
+        av_len = 0
+    #scheduler.step()
     #prob = prob * 0.99
 #torch.save(Q_Net.state_dict(), r'C:\Users\pmuralikrishnan\Desktop\Python\New folder\New folder\MountainCar_Q_Policy')
